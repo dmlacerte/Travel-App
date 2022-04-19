210d5feb03896a70c3e4d88471d938b02c9fd619
dmlacerte <dmlacerte@gmail.com>
2022-04-18 22:21:24 -0400

    Debugging server connection issue

--- index.js
+++ index.js
@@ -16,6 +16,5 @@ app.get("/", (req, res) => {
 
 app.use('/states', travelController);
 
-const port = 3000;
-// process.env.PORT ||
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is connected to port ${port}`));
