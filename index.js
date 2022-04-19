064eeffe408948e84c343805a856c68ed6e9e788
dmlacerte <dmlacerte@gmail.com>
2022-04-18 22:49:28 -0400

    Debugging server connection issue

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/frontend/index.html");
 });
 
-const Activity = require('./backend//models/activities-model.js');
+// const Activity = require('./backend//models/activities-model.js');
 
-app.get('/:state', (req, res, next) => {
-    Activity.find({ state: req.params.state })
-        // .then(activities => res.json(activities))
-        .then(activities => {
-            res.render('state', { activities })
-        })
-        .catch(next);
-});
+// app.get('/:state', (req, res, next) => {
+//     Activity.find({ state: req.params.state })
+//         // .then(activities => res.json(activities))
+//         .then(activities => {
+//             res.render('state', { activities })
+//         })
+//         .catch(next);
+// });
 
-// app.use("https://dml-mytravelapp.herokuapp.com", travelController);
+app.use("https://dml-mytravelapp.herokuapp.com", travelController);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is connected to port ${port}`));
