766233db378520571ea68b27e4f83727b4ec0572
dmlacerte <dmlacerte@gmail.com>
2022-04-18 22:35:13 -0400

    Debugging server connection issue

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/frontend/index.html");
 });
 
-app.use("https://dml-mytravelapp.herokuapp.com", travelController);
+app.get('/:state', (req, res, next) => {
+    res.send('Entered state route')
+    // Activity.find({ state: req.params.state })
+    //     // .then(activities => res.json(activities))
+    //     .then(activities => {
+    //         console.log(activities)
+    //         res.render('state', { activities })
+    //     })
+    //     .catch(next);
+});
+
+// app.use("https://dml-mytravelapp.herokuapp.com", travelController);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is connected to port ${port}`));
